55b2b411de740e121ee565e449fca928feb9b993
robin hardjowijono <robinhardjowijono@hotmail.com>
2021-04-26 16:25:20 +0200

    Stap 11/12/13/14

--- assets/js/game.js
+++ assets/js/game.js
@@ -81,12 +81,27 @@ class Game {
         player.velocity.y += this.keys[40] === true ? 400 : 0;
         break;
       case 2: // Computer player (rechts)
-        player.position.y = ball.position.y;
+        if (player.locked) {
+          if (--player.stickyFrames === 0) {
+            player.velocity.y = 0;
+            player.locked = false;
+            // console.log('unlock');
+          }
+          return;
+        } else {
+          player.velocity.y = 0;
+          player.velocity.y += ball.position.y < player.top ? -400 : 0;
+          player.velocity.y += ball.position.y > player.bottom ? 400 : 0;
+          player.stickyFrames = getRandomNumBetween(5, 20);
+          player.locked = true;
+          // console.log('lock');
+        }
         break;
     }
   }
 
   update(deltatime) {
+    if (this.ball.locked) return;
     this.ball.position.x += this.ball.velocity.x * deltatime;
     this.ball.position.y += this.ball.velocity.y * deltatime;
 
@@ -109,13 +124,97 @@ class Game {
     }
 
     //Bal boven en beneden tegenhouden
-    if (this.ball.bottom > this.canvas.height - 10 || this.ball.top < 10) {
-      this.ball.velocity.y = -this.ball.velocity.y;
+    if (ball.bottom > this.canvas.height - 10 || ball.top < 10) {
+      ball.velocity.y = -ball.velocity.y;
     }
-    if (this.ball.right > this.canvas.width || this.ball.left < 0) {
-      this.ball.velocity.x = -this.ball.velocity.x;
+
+    if (ball.out) return;
+
+    if (
+      ball.left + ball.velocity.y * deltatime < players[0].right ||
+      ball.right - ball.velocity.y * deltatime > players[1].left
+    ) {
+      // Check op welke speler de focus moet liggen voor wat betreft de aankomende botsing
+      const player =
+        ball.position.x < this.canvas.width / 2 ? players[0] : players[1];
+      // Check of er een botsing gaat plaatsvinden in de volgende frame (AABB)
+      if (this.collide(ball, player, deltatime)) {
+        // Check of de botsing frontaal was
+        if (ball.bottom > player.top && ball.top < player.bottom) {
+          console.log("frontale botsing gedetecteerd");
+          // Positioneer bal op zijkant van spelerbedje
+          ball.position.x =
+            player.id === 1
+              ? player.right + ball.size.y / 2
+              : player.left - ball.size.y / 2;
+          // Laat bal stuiteren op de x-as
+          ball.velocity.x = -ball.velocity.x;
+
+          const ballY = ball.position.y | 0;
+          const playerY = player.position.y | 0;
+          const distance =
+            ballY < playerY
+              ? Math.abs(ballY - playerY)
+              : -Math.abs(ballY - playerY);
+
+          console.log("afstand vanaf midden van bedje: " + distance);
+
+          if (distance !== 0)
+            ball.setAngle(player.id === 1 ? distance : -distance + 180);
+
+          Math.floor();
+          Math.random();
+          Math.ceil();
+          Math.PI;
+
+          // Verhoog de snelheid van de bal met 10%
+          ball.setSpeed(ball.speed * 1.1);
+
+          // Check of de botsing van bovenaf was
+        } else if (ball.position.y < player.position.y) {
+          console.log("botsing aan de bovenkant van speler gedetecteerd");
+          ball.position.y = player.top - ball.size.y / 2;
+          ball.velocity.y =
+            player.velocity.y < 0 && player.velocity.y < ball.velocity.y
+              ? player.velocity.y * 1.1
+              : -ball.velocity.y;
+          // Check of de botsing van onderaf was
+        } else if (ball.position.y > player.position.y) {
+          console.log("botsing aan de onderkant van speler gedetecteerd");
+          ball.position.y = player.bottom + ball.size.y;
+          ball.velocity.y =
+            player.velocity.y > 0 && player.velocity.y > ball.velocity.y
+              ? player.velocity.y * 1.1
+              : -ball.velocity.y;
+        }
+      }
+      //check of de bal uit gaat
+      if (ball.right < 0 || ball.left > this.canvas.width) {
+        this.hud.addScore(player.id === 1 ? 2 : 1);
+        ball.out = true;
+        setTimeout(() => {
+          this.ball.reset();
+        }, 1000);
+      }
     }
   }
+
+  collide(rect1, rect2, dt) {
+    if (
+      rect1.left + rect1.velocity.x * dt <
+        rect2.right + rect2.velocity.x * dt &&
+      rect1.right + rect1.velocity.x * dt >
+        rect2.left + rect2.velocity.x * dt &&
+      rect1.top + rect1.velocity.y * dt <
+        rect2.bottom + rect2.velocity.y * dt &&
+      rect1.bottom + rect1.velocity.y * dt > rect2.top + rect2.velocity.y * dt
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   draw() {
     this.drawRectangle(this.context, this.ball);
 
